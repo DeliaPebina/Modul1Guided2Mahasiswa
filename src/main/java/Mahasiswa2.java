61b0bdbc8b6cedf40805977e096a82c94ca1b5aa
DeliaPebina <deliapebina@gmail.com>
2023-10-11 11:48:58 +0700

    Modul 1 Guided yang terakhirrr :))))))))

---
+++ src/main/java/Mahasiswa2.java
@@ -0,0 +1,8 @@
+public class Mahasiswa2 {
+    public static void main(String[] args)
+    {
+        Mahasiswa M = new Mahasiswa (); //Pembentukan Objek
+        M.inputData(); // Pemanggilan method
+        M.display();
+    }
+}
